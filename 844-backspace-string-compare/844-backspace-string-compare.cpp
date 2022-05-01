08aa2631e9e3ebb2037144845d7f00c02e17fad3
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-05-01 23:54:47 +0530

    Time: 4 ms (28.12%), Space: 6.3 MB (68.92%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,30 +3,17 @@ public:
     bool backspaceCompare(string s, string t) {
         string s1="";
         string t1="";
-        int i=0,j=0;
-        for(i=0,j=0;i<s.size()&&j<t.size();i++,j++){
+        for(int i=0;i<s.size();i++){
             if(s[i]=='#'){
                if(s1.size()>0) s1.pop_back();
                }   
             else s1+=s[i];
-            if(t[j]=='#'){
+        }
+        for(int i=0;i<t.size();i++){
+            if(t[i]=='#'){
                if(t1.size()>0) t1.pop_back();
                }   
-            else t1+=t[j];
-        }
-        while(i<s.size()){
-            if(s[i]=='#'){
-               if(s1.size()>0) s1.pop_back();
-               }   
-            else s1+=s[i];
-            i++;
-        }
-        while(j<t.size()){
-            if(t[j]=='#'){
-               if(t1.size()>0) t1.pop_back();
-               }   
-            else t1+=t[j];
-            j++;
+            else t1+=t[i];
         }
         return s1==t1;
     }
