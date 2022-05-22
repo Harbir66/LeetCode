30abaccfe4d345c9aca5a6a5c9eb56d03699cc7c
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-05-22 17:48:46 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (37.07%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -6,16 +6,12 @@ public:
         }
         int l=0;
         int i=0;
-        cout<<"I "<<i<<" ";
         while(nums[i]==val){
             i++;
             if(i>=nums.size()){
                 return 0;
             }
-            cout<<i<<" ";
         }
-        cout<<endl;
-        
         for(i;i<nums.size();i++){
             if(nums[i]!=val){
                 nums[l]=nums[i];
