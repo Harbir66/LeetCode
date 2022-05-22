6b8736a5c0a410a7b15e572174d10fa040d169ca
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-05-22 17:39:11 +0530

    Time: 15 ms (63.85%), Space: 18.4 MB (74.92%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,30 +2,14 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int unique=1;
-        int num=nums[0];
-        int pos=-1;
-        cout<<nums[0]<<" ";
+        int l=1;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]==num){
-                cout<<"same ";
-                nums[i]=-101;
-                if(pos==-1) pos=i;
-            }
-            else if(nums[i]!=-101){
-                num=nums[i];
-                cout<<num<<" ";
+            if(nums[i-1]!=nums[i]){
+                nums[l]=nums[i];
                 unique++;
-                if(pos!=-1){
-                    nums[i]=-101;
-                    nums[pos]=num;
-                    while(nums[pos]!=-101 && pos < nums.size()){
-                        pos++;
-                    }
-                }
-                
+                l++;
             }
         }
-        cout<<endl;
         
         return unique;
     }
