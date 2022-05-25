da56764e7f0cada9151561d08c12d1e8b514fa0d
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-05-25 13:10:04 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (85.44%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int i=0;
+        int curr=digits[i]+1;
+        cout<<"Curr :"<<curr<<'\n';
+        
+        if(curr==10){
+            while(curr==10){
+                digits[i]=0;
+                i++;
+                if(i==digits.size()) digits.push_back(0);
+                curr=digits[i]+1;
+                cout<<"Curr :"<<curr<<'\n';
+            }
+        }
+        digits[i]=curr;
+        
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
