67eec8b55d5f48297a8aca192aad0a7d52fd6484
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-06-28 15:01:22 +0530

    Time: 268 ms (5.05%), Space: 17.5 MB (21.80%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        map<char,int> m;
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++;
+        }
+        int res=0;
+        map<int,bool> m2;
+        for(auto i=m.begin();i!=m.end();i++){
+            cout<<i->first<<" : "<<i->second<<"\n";
+            if(m2[i->second]==0){
+                m2[i->second]=1;
+            }
+            else{
+                cout<<i->first<<"\n";
+                int num=i->second;
+                while(m2[num]!=0 && num >0){
+                    num--;
+                }
+                int temp=i->second-num;
+                res+=temp;
+                m2[num]=1;
+            }
+        }
+        return res;
+    }
+};
