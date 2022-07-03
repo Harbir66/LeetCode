7ae33c32ad65e3b67d104fd03813e3915c491621
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-03 11:10:00 +0530

    Time: 9 ms (32.92%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         if(nums.size()==1) return 1;
-        // if(nums.size()==2 && nums[0]==nums[1]) return 1;
         int result=1;
         int count=1;
         int pos=-1;
@@ -19,21 +18,12 @@ public:
                     count++;
                     pos=0;
                 }
-                // else{
-                //     result=max(result,count);
-                //     // count=2;
-                // }
             }
             else{
                 if(nums[i-1]<nums[i]){
                     count++;
                     pos=1;
                 }
-                // else{
-                //     result=max(result,count);
-                //     // count=2;
-                // }
-                
             }
             cout<<pos<<"\n";
         }
