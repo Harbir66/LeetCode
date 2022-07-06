e3cbc956ceefa317be65d65d56713ff4a7895669
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-06 19:13:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    // int num=31;
+    vector<int> temp=vector<int>(31,-1);
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        if(temp[n]!=-1) return temp[n];
+        else{
+            if(n==0) {
+                temp[n]=0;
+                return 0;
+            }
+            if(n==1){
+                temp[n]=1;
+                return 1;
+            }
+            temp[n]=fib(n-1)+fib(n-2);
+            return temp[n];
+            
+        }
     }
 };
