5502d0ab4e599a1541c82da63fe5ed8e2dcf0462
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-12 16:21:07 +0530

    Time: 580 ms (39.01%), Space: 10.1 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int a,b,c,d;
+    bool solve(vector<int>& matchsticks,int i){
+        if(i==matchsticks.size()){
+            if(a==0 && b==0 && c==0 && d==0) return true;
+            else return false;
+        }
+        
+        if(matchsticks[i]<=a){
+            a-=matchsticks[i];
+            if(solve(matchsticks,i+1)) return true;
+            a+=matchsticks[i];
+        }
+        if(matchsticks[i]<=b){
+            b-=matchsticks[i];
+            if(solve(matchsticks,i+1)) return true;
+            b+=matchsticks[i];
+        }
+        if(matchsticks[i]<=c){
+            c-=matchsticks[i];
+            if(solve(matchsticks,i+1)) return true;
+            c+=matchsticks[i];
+        }
+        if(matchsticks[i]<=d){
+            d-=matchsticks[i];
+            if(solve(matchsticks,i+1)) return true;
+            d+=matchsticks[i];
+        }
+        
+        return false;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+        if(matchsticks.size()<4) return false;
+        int sum=accumulate(matchsticks.begin(),matchsticks.end(),0);
+        if(sum%4!=0) return false;
+        sum/=4;
+        a=sum;
+        b=sum;
+        c=sum;
+        d=sum;
+        sort(matchsticks.rbegin(),matchsticks.rend());
+        
+        return solve(matchsticks,0);
+    }
+};
