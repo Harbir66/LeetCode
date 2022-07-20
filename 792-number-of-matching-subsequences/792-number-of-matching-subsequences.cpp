10b86a06e6e62fddfd16015252c2f4ff3aaabb0b
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-20 22:43:42 +0530

    Time: 424 ms (43.96%), Space: 36.2 MB (91.09%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-//         map<string,int> mp;
-        
-//         int res=0;
-//         for(int i=0;i<words.size();++i){
-//             if(mp.find(words[i])!=mp.end()){
-//                 res+=mp[words[i]];
-//             }
-//             else{
-//                 string t=words[i];
-//                 for(int j=s.size()-1;j>=0;--j){
-//                     if(s[j]==t.back()){
-//                         t.pop_back();
-//                     }
-//                     if(t.size()==0){
-//                         mp[words[i]]=1;
-//                         res++;
-//                         break;
-//                     }
-//                 }
-//                 if(mp[words[i]]==0)
-//                     mp[words[i]]=0;    
-//             }
-            
-//         }
-//         // for(auto i=mp.begin();i!=mp.end();i++){
-//         //     cout<<i->first<<" : "<<i->second<<"\n";
-//         // }
-//         return res;
         unordered_map<string,int> mp;
-        for(int i=0;i<words.size();i++){
+        for(int i=0;i<words.size();++i){
             mp[words[i]]++;
         }
-        // for(auto i=mp.begin();i!=mp.end();i++){
-        //     cout<<i->first<<" : "<<i->second<<"\n";
-        // }
         int res=0;
         
-        for(auto i=mp.begin();i!=mp.end();i++){
+        for(auto i=mp.begin();i!=mp.end();++i){
             string temp=i->first;
             int k=0,j=0;
-            // for(int j=0;j<s.size();j++){
-            //     if(s[j]==temp[k]) k++;
-            // }
             while(j<s.size() && k<temp.size()){
                 if(s[j]==temp[k]){
-                    j++;
-                    k++;
+                    ++j;
+                    ++k;
                 }
                 else{
-                    j++;
+                    ++j;
                 }
             }
             if(k==temp.size()){
