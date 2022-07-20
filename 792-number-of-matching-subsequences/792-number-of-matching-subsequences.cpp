bed84ab993eb0fce68864b2c202d83e2bb17b916
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-20 22:44:22 +0530

    Time: 319 ms (61.19%), Space: 36 MB (93.77%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -7,16 +7,16 @@ public:
         }
         int res=0;
         
-        for(auto i=mp.begin();i!=mp.end();++i){
+        for(auto i=mp.begin();i!=mp.end();i++){
             string temp=i->first;
             int k=0,j=0;
             while(j<s.size() && k<temp.size()){
                 if(s[j]==temp[k]){
-                    ++j;
-                    ++k;
+                    j++;
+                    k++;
                 }
                 else{
-                    ++j;
+                    j++;
                 }
             }
             if(k==temp.size()){
