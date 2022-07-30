b58d5eff42d6641f39558f591d07c82b13ff4947
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-30 12:36:29 +0530

    Time: 202 ms (86.74%), Space: 57.2 MB (96.63%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        map<char,int> mp1;
-        map<char,int> mp2;
+        // map<char,int> mp1;
+        // map<char,int> mp2;
+        
+        int mp2[26]={0,};
         vector<string> ans;
         for(int i=0;i<words2.size();++i){
+            int mp1[26]={0,};
             for(int j=0;j<words2[i].size();++j){
-                ++mp1[words2[i][j]];
-                mp2[words2[i][j]]=max(mp2[words2[i][j]],mp1[words2[i][j]]);
+                ++mp1[words2[i][j]-'a'];
+                mp2[words2[i][j]-'a']=max(mp2[words2[i][j]-'a'],mp1[words2[i][j]-'a']);
             }
-            
-            mp1.clear();
         }
         for(int i=0;i<words1.size();++i){
+            int mp1[26]={0,};
             ans.push_back(words1[i]);
             for(int j=0;j<words1[i].size();++j){
-                ++mp1[words1[i][j]];
+                ++mp1[words1[i][j]-'a'];
             }
-            for(auto j=mp2.begin();j!=mp2.end();++j){
-                if(mp1.find(j->first)==mp1.end()) {ans.pop_back();break;}
-                else if(mp1[j->first]<j->second) {ans.pop_back();break;}
+            for(int j=0;j<26;++j){
+                if(mp2[j]>mp1[j]) {ans.pop_back();break;}
             }
-            mp1.clear();
+            // for(auto j=mp2.begin();j!=mp2.end();++j){
+            //     if(mp1[])
+            //     if(mp1.find(j->first)==mp1.end()) {ans.pop_back();break;}
+            //     else if(mp1[j->first]<j->second) {ans.pop_back();break;}
+            // }
         }
         return ans;
     }
