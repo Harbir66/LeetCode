537654d2e56680639a40c6c72811442fdec72006
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-07-30 14:30:07 +0530

    Time: 42 ms (5.20%), Space: 13.3 MB (52.70%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,23 +4,34 @@ public:
         
         int n=matrix.size();
         int m=matrix[0].size();
-
+        int ind=(n*m)-1;
         vector<int> rows(n,0);
         vector<int> cols(m,0);
-        for(int i=0;i<n;++i){
-            // if(rows[i]) continue;
-            for(int j=0;j<m;++j){
-                // if(cols[j]) continue;
-                if(matrix[i][j]==0){
-                    rows[i]=1;
-                    cols[j]=1;
-                }
+        for(int i=0;i<=ind;++i){
+            int row=i/m;
+            int col=i%m;
+            if(matrix[row][col]==0){
+                rows[row]=1;
+                cols[col]=1;
             }
         }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(rows[i]||cols[j]) matrix[i][j]=0;
-            }
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<m;++j){
+        //         if(matrix[i][j]==0){
+        //             rows[i]=1;
+        //             cols[j]=1;
+        //         }
+        //     }
+        // }
+        for(int i=0;i<=ind;++i){
+            int row=i/m;
+            int col=i%m;
+            if(rows[row]||cols[col]) matrix[row][col]=0;
         }
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<m;++j){
+        //         if(rows[i]||cols[j]) matrix[i][j]=0;
+        //     }
+        // }
     }
 };
