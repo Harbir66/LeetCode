3abc1252e7f4637c5af4a88491824a1acd2b7b7e
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-08-03 20:19:25 +0530

    Time: 218 ms (29.41%), Space: 37.7 MB (77.01%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,22 +1,31 @@
 class MyCalendar {
      priority_queue<pair<int,int>> pq;
+    vector<pair<int,int>> v;
 public:
     MyCalendar() {
         // pq;
+        v.clear();
     }
     
     bool book(int start, int end) {
         priority_queue<pair<int,int>> temp=pq;
-        while(!temp.empty()){
-            int s=temp.top().first;
-            int e=temp.top().second;
+//         while(!temp.empty()){
+//             int s=temp.top().first;
+//             int e=temp.top().second;
+//             if((end>s && end<=e) || (start>=s && start<e)|| (start<=s && end>=e)) {
+//                 return false;
+//             }
+            
+//             temp.pop();
+//         }
+        for(int i=0;i<v.size();i++){
+            int s=v[i].first;
+            int e=v[i].second;
             if((end>s && end<=e) || (start>=s && start<e)|| (start<=s && end>=e)) {
                 return false;
             }
-            
-            temp.pop();
         }
-        pq.push({start,end});
+        v.push_back({start,end});
         return true;
     }
 };
