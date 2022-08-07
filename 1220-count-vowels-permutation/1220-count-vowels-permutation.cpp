b97d28af9e677c1f08b502dc5bbbb131050e85fa
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-08-07 10:18:09 +0530

    Time: 111 ms (41.12%), Space: 27.3 MB (32.40%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<long long>> dp(n,vector<long long >(5,1));
         int mod=1e9+7;
         long long sum=0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;++i){
             dp[i][0]=((dp[i-1][1]+dp[i-1][2]+dp[i-1][4]))%mod;
             dp[i][1]=((dp[i-1][0]+dp[i-1][2]))%mod;
             dp[i][2]=((dp[i-1][1]+dp[i-1][3]))%mod;
