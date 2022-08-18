c2166bfd60713c353427b79918f28c740d0db13f
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-08-18 10:58:53 +0530

    Time: 643 ms (6.23%), Space: 83 MB (10.98%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    static bool cmp(pair<int,int> a,pair<int,int> b){
+        return a.second<b.second;
+    }
+    int minSetSize(vector<int>& arr) {
+        int n=arr.size();
+        map<int,int> mp;
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;++i){
+            mp[arr[i]]++;
+        }
+        int half=n/2;
+        for(auto i=mp.begin();i!=mp.end();++i){
+            // cout<<i->first<<" : "<<i->second<<endl;
+            v.push_back({i->first,i->second});
+        }
+        sort(v.begin(),v.end(),cmp);
+        // for(auto i=v.begin();i!=v.end();++i){
+        //     cout<<i->first<<" : "<<i->second<<endl;
+        // }
+        auto i=v.rbegin();
+        int res=0;
+        while(half>0){
+            res++;
+            half-=i->second;
+            i++;
+        }
+        return res;
+    }
+};
