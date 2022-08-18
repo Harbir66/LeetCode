17723716a1b42d393ecbd3928d11f8f227e563ef
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-08-18 11:03:41 +0530

    Time: 293 ms (68.06%), Space: 80.2 MB (39.23%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int> a,pair<int,int> b){
-        return a.second<b.second;
-    }
     int minSetSize(vector<int>& arr) {
         int n=arr.size();
         map<int,int> mp;
-        vector<pair<int,int>> v;
+        vector<int> v;
         for(int i=0;i<n;++i){
             mp[arr[i]]++;
         }
         int half=n/2;
         for(auto i=mp.begin();i!=mp.end();++i){
-            v.push_back({i->first,i->second});
+            v.push_back(i->second);
         }
-        sort(v.begin(),v.end(),cmp);
+        sort(v.begin(),v.end());
         auto i=v.rbegin();
         int res=0;
         while(half>0){
             res++;
-            half-=i->second;
+            half-=*i;
             i++;
         }
         return res;
