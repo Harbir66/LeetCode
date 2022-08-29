ad18d692650bb14313b9e48bc4bae34a291092d6
Harbir Singh <62716902+Harbir66@users.noreply.github.com>
2022-08-29 11:57:08 +0530

    Time: 70 ms (29.23%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> visited;
+    // vector<vector<int>> visited;
     int ans=0;
     void dfs(vector<vector<char>>& grid,int i,int j){
         // cout<<"I"<<i<<" "<<"J"<<j<<endl;
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size())
             return;
-        if(grid[i][j]=='0' || visited[i][j]) return;
-        visited[i][j]=1;
+        if(grid[i][j]=='0' || grid[i][j]=='V') return;
+        // visited[i][j]=1;
+        grid[i][j]='V';
         dfs(grid,i,j+1);
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
         dfs(grid,i,j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        visited.resize(grid.size(),vector<int>(grid[0].size(),0));
+        // visited.resize(grid.size(),vector<int>(grid[0].size(),0));
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1'){
                     ans++;
                     dfs(grid,i,j);    
                 }
